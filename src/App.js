e775b9dced73ed2bcee18f9c5449f168b444950e
oktay <oktaycolakoglu@gmail.com>
2022-04-04 00:24:04 +0300

    add theme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {
   ChakraProvider, Heading,
 } from '@chakra-ui/react';
-
+import theme from './lib/theme';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Heading>Hello World</Heading>
     </ChakraProvider>
   );
---
+++ src/lib/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const globalStyles = {}
+
+const theme = extendTheme({
+  styles: {
+    global: globalStyles,
+  }
+})
+
+export default theme
+  
