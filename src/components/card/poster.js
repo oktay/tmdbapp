f1dfe3c4dc2c1dd608166c0f349c1c1d1f264ead
oktay <oktaycolakoglu@gmail.com>
2022-06-19 13:45:17 +0300

    fallback poster

--- src/components/card/poster.js
+++ src/components/card/poster.js
@@ -5,11 +5,12 @@ import { useImage } from '../../lib/tmdb';
 function Poster({ path, size }) {
   const [url] = useImage({ path, size: size || '500' });
   const [loaded, setLoaded] = useState(false);
+  const [src, setSrc] = useState(url)
 
   return (
     <AspectRatio ratio={2 / 3}>
       <Skeleton isLoaded={loaded}>
-        <Image src={url} onLoad={() => setLoaded(true)} />
+        <Image src={src} onLoad={() => setLoaded(true)} onError={() => setSrc('/fallback-poster.png')} />
       </Skeleton>
     </AspectRatio>
   );
